7bb0ddd83a17ce02e903cf579abe76f5eff911bd
Hapsik1234 <84462088+Hapsik1234@users.noreply.github.com>
2023-05-08 21:11:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function change() {
+function changePitch() {
 	function backup() {}
 	backup = pitchDown;
 	pitchDown = pitchUp;
 	pitchUp = backup;	
 }
+
+function correctRoll() {
+  var num = 2;
+  console.log(fixedRotationZ);
+  if (fixedRotationZ>0) {
+    console.log("Larger than zero");
+    rollRight();
+    while (fixedRotationZ>0) {
+      console.log("Awaiting to complete rotation manouver " + fixedRotationZ.toString());
+      dnt();
+    }
+    rollLeft();
+  }
+}
+
+function dnt() {}
