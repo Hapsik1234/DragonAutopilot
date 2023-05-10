78d75ceb14e6c52d3f64058d3c915d218674058e
Hapsik1234 <84462088+Hapsik1234@users.noreply.github.com>
2023-05-10 22:24:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function correctRoll() {
   console.log(fixedRotationZ);
   if (fixedRotationZ>0) {
     console.log("Larger than zero");
-    test();
+    Window.rollRight();
     while (fixedRotationZ>0) {
       console.log("Awaiting to complete rotation manouver " + fixedRotationZ.toString());
       dnt();
